267f0f197cdf8ce25385ec7e5d9ae0b65dcc8594
Aleksandar Konatar <aleksandar.k03@gmail.com>
2026-02-14 19:38:56 +0100

    display generated images

---
+++ src/Aco228.BlazorShared/Models/UIModel.cs
@@ -0,0 +1,27 @@
+﻿namespace Aco228.BlazorShared.Models;
+
+public class UIModel<TEntry>
+{
+    public bool IsVisible { get; set; }
+    public bool IsSelected { get; set; }
+    public TEntry Entry { get; set; }
+}
+
+public static class UIModelExtensions
+{
+    public static List<UIModel<TEntry>> ToUIModels<TEntry>(
+        this IEnumerable<TEntry> entries, 
+        bool isVisibleByDefault = true)
+    {
+        var result = new List<UIModel<TEntry>>();
+        foreach (var entry in entries)
+            result.Add(new()
+            {
+                Entry = entry,
+                IsVisible = isVisibleByDefault,
+                IsSelected = false
+            });
+
+        return result;
+    }
+}
