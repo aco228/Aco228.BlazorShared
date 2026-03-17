3ae0ec93abd9e42ce34a2a337fb21c35247a8aff
Aleksandar Konatar <aleksandar.k03@gmail.com>
2026-03-17 13:02:59 +0100

    bug fixes

---
+++ src/Aco228.BlazorShared/Code/ISidebarMediator.cs
@@ -0,0 +1,20 @@
+﻿namespace Aco228.BlazorShared.Code;
+
+public interface ISidebarMediator
+{
+    public bool IsVisible { get; set; }
+    event EventHandler<bool> OnSidebarVisibilityChanged;
+    void ChangeSidebarVisibility(bool? isVisible = null);
+}
+
+public class SidebarMediator : ISidebarMediator
+{
+    public bool IsVisible { get; set; } = true;
+    public event EventHandler<bool>? OnSidebarVisibilityChanged;
+    
+    public void ChangeSidebarVisibility(bool? isVisible = null)
+    {
+        IsVisible = isVisible ?? !IsVisible;
+        OnSidebarVisibilityChanged?.Invoke(this, IsVisible);
+    }
+}
